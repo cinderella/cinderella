44f93a3e0c4853a805245e8bd99c7b277114d258
Shane Witbeck <shane@digitalsanctum.com>
2012-10-09 13:01:49 -0400

    removed comments

--- cinderella-web/src/main/java/io/cinderella/service/CinderellaService.java
+++ cinderella-web/src/main/java/io/cinderella/service/CinderellaService.java
@@ -9,29 +9,24 @@ import com.amazon.ec2.*;
 public interface CinderellaService {
 
     /**
-     * ec2-describe-availability-zones -U http://localhost:8080/api/ -O AKIAJLRNZIQQ37IVPP3Q -W 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J -v --debug --request-timeout 120
      * @param describeAvailabilityZones
      * @return
      */
     DescribeAvailabilityZonesResponse describeAvailabilityZones(DescribeAvailabilityZones describeAvailabilityZones);
 
     /**
-     * ec2-describe-regions -U http://localhost:8080/api/ -O AKIAJLRNZIQQ37IVPP3Q -W 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J -v --debug --request-timeout 120
      * @param describeRegions
      * @return
      */
     DescribeRegionsResponse describeRegions(DescribeRegions describeRegions);
 
     /**
-     * ec2-describe-images -U http://localhost:8080/api/ -O AKIAJLRNZIQQ37IVPP3Q -W 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J -v --debug --request-timeout 120
      * @param describeImages
      * @return
      */
     DescribeImagesResponse describeImages(DescribeImages describeImages);
 
     /**
-     * euca-describe-instances -U http://localhost:8080/api/ -a AKIAJLRNZIQQ37IVPP3Q -s 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J --debug
-     * ec2-describe-instances -U http://localhost:8080/api/ -O AKIAJLRNZIQQ37IVPP3Q -W 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J -v --debug --request-timeout 120
      * @param describeInstances
      * @return
      */
@@ -39,7 +34,6 @@ public interface CinderellaService {
 
 
     /**
-     * ec2-describe-group -U http://localhost:8080/api/ -O AKIAJLRNZIQQ37IVPP3Q -W 5YVvxvaPCcJ2vr5/ZyRsKKuvo6WJvV1KMnUYm+7J -v --debug --request-timeout 120
      * @param describeSecurityGroups
      * @return
      */
